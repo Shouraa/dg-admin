a810a5a485e08569880403a9019332490a3b9349
Shouraa <shorajafar@gmail.com>
2021-09-06 18:25:16 +0300

    some changes

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import useStyles from './styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { makeStyles } from '@material-ui/core/styles';
 
 import AppDrawer from '../components/AppDrawer/AppDrawer';
 import AppBarCustom from '../components/Header/AppBarCustom/AppBarCustom';
 import AppContent from '../components/AppContent/AppContent';
 
-const DefaultLayout = () => {
-  const classes = useStyles();
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+  },
+}));
+
+const DefaultLayout = (props) => {
+  const classes = useStyles(props);
   return (
     <div className={classes.root}>
+      <CssBaseline />
+      <AppBarCustom />
       <AppDrawer />
-      <main className={classes.content}>
-        <AppBarCustom />
-
-        <div className={classes.appLayout}>
-          <AppContent />
-        </div>
-      </main>
+      <AppContent />
     </div>
   );
 };
