59d3df5c7edfb666940f8a44e614dbf5ef66f9e5
Ehsan Khakifirooz <ehsan.khf@gmail.com>
2021-09-30 20:26:11 +0100

    fix: fixed the linter and added the redux logger middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 // import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 import rankReducer from '../reducers/rankReducer';
 
@@ -8,10 +9,12 @@ const rootReducer = combineReducers({
   ranks: rankReducer,
 });
 
+const middlewares = [thunk, logger];
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(...middlewares))
 );
 
 export default store;
